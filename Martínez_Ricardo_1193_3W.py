b07eb2fe2447b71b1084b15dcbab781921b1e447
Ricardo Martínez <a.23308051281193@cbtis128.edu.mx>
2024-09-26 14:38:58 -0600

    Add files via upload

---
+++ Martínez_Ricardo_1193_3W.py
@@ -0,0 +1,12 @@
+# Exámen 1er parcial.
+print (" ")
+print ("Martínez Estrada Ricardo / NO. de control: 1193 / Grupo 3°W.")
+print (" ")
+
+ap = "Martinez"
+am = "Estrada"
+no = "Ricardo"
+espacio = " "
+nc = ap+espacio+am+espacio+no
+
+print (nc.upper())
